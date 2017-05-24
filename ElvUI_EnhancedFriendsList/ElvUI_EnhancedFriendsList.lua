5c797cbd3d9098d80bea8a24a850702b41083eb1
Appolyonn <jellefenxd@gmail.com>
2017-05-24 15:51:43 +0300

    ElvUI_EnhancedFriendsList

---
+++ ElvUI_EnhancedFriendsList/ElvUI_EnhancedFriendsList.lua
@@ -0,0 +1,317 @@
+local E, L, V, P, G = unpack(ElvUI)
+local EFL = E:NewModule("EnhancedFriendsList")
+local EP = LibStub("LibElvUIPlugin-1.0");
+local addonName = ...;
+local LSM = LibStub("LibSharedMedia-3.0", true)
+
+local pairs = pairs
+local format = format
+
+local IsChatAFK = IsChatAFK
+local IsChatDND = IsChatDND
+local GetFriendInfo, GetQuestDifficultyColor = GetFriendInfo, GetQuestDifficultyColor
+local LEVEL = LEVEL
+local RAID_CLASS_COLORS = RAID_CLASS_COLORS
+
+local EnhancedOnline = "Interface\\AddOns\\ElvUI_EnhancedFriendsList\\Media\\Textures\\StatusIcon-Online"
+local EnhancedOffline = "Interface\\AddOns\\ElvUI_EnhancedFriendsList\\Media\\Textures\\StatusIcon-Offline"
+local EnhancedAfk = "Interface\\AddOns\\ElvUI_EnhancedFriendsList\\Media\\Textures\\StatusIcon-Away"
+local EnhancedDnD = "Interface\\AddOns\\ElvUI_EnhancedFriendsList\\Media\\Textures\\StatusIcon-DnD"
+
+local Locale = GetLocale()
+
+-- Profile
+P["enhanceFriendsList"] = {
+	["enhancedTextures"] = true,
+	["enhancedName"] = true,
+	["enhancedZone"] = false,
+	["showClass"] = false,
+	["levelColor"] = false,
+	["sameZone"] = true,
+	["nameFont"] = "PT Sans Narrow",
+	["nameFontSize"] = 12,
+	["nameFontOutline"] = "NONE",
+	["zoneFont"] = "PT Sans Narrow",
+	["zoneFontSize"] = 12,
+	["zoneFontOutline"] = "NONE"
+};
+
+-- Options
+local function ColorizeSettingName(settingName)
+	return format("|cff1784d1%s|r", settingName);
+end
+
+function EFL:InsertOptions()
+	E.Options.args.enhanceFriendsList = {
+		order = 51.1,
+		type = "group",
+		name = ColorizeSettingName(L["Enhanced Friends List"]),
+		get = function(info) return E.db.enhanceFriendsList[ info[#info] ] end,
+		set = function(info, value) E.db.enhanceFriendsList[ info[#info] ] = value; end,
+		args = {
+			header = {
+				order = 1,
+				type = "header",
+				name = L["Enhanced Friends List"]
+			},
+			general = {
+				order = 2,
+				type = "group",
+				name = L["General"],
+				guiInline = true,
+				args = {
+					enhancedTextures = {
+						order = 1,
+						type = "toggle",
+						name = L["Enhanced Status"],
+						set = function(info, value) E.db.enhanceFriendsList.enhancedTextures = value; EFL:EnhanceFriends() EFL:FriendDropdownUpdate() end
+					},
+					enhancedName = {
+						order = 2,
+						type = "toggle",
+						name = L["Enhanced Name"],
+						set = function(info, value) E.db.enhanceFriendsList.enhancedName = value; EFL:EnhanceFriends() end
+					},
+					enhancedZone = {
+						order = 3,
+						type = "toggle",
+						name = L["Enhanced Zone"],
+						set = function(info, value) E.db.enhanceFriendsList.enhancedZone = value; EFL:EnhanceFriends() end
+					},
+					showClass = {
+						order = 4,
+						type = "toggle",
+						name = L["Show Class Text"],
+						set = function(info, value) E.db.enhanceFriendsList.showClass = value; EFL:EnhanceFriends() end
+					},
+					levelColor = {
+						order = 5,
+						type = "toggle",
+						name = L["Level Range Color"],
+						set = function(info, value) E.db.enhanceFriendsList.levelColor = value; EFL:EnhanceFriends() end
+					},
+					sameZone = {
+						order = 6,
+						type = "toggle",
+						name = L["Same Zone Color"],
+						set = function(info, value) E.db.enhanceFriendsList.sameZone = value; EFL:EnhanceFriends() end
+					}
+				}
+			},
+			nameFont = {
+				order = 3,
+				type = "group",
+				name = L["Name Text Font"],
+				guiInline = true,
+				args = {
+					nameFont = {
+						order = 1,
+						type = "select", dialogControl = "LSM30_Font",
+						name = L["Font"],
+						values = AceGUIWidgetLSMlists.font,
+						set = function(info, value) E.db.enhanceFriendsList.nameFont = value; EFL:EnhanceFriends() end
+					},
+					nameFontSize = {
+						order = 2,
+						type = "range",
+						name = L["Font Size"],
+						min = 6, max = 22, step = 1,
+						set = function(info, value) E.db.enhanceFriendsList.nameFontSize = value; EFL:EnhanceFriends() end
+					},
+					nameFontOutline = {
+						order = 3,
+						type = "select",
+						name = L["Font Outline"],
+						desc = L["Set the font outline."],
+						values = {
+							["NONE"] = L["None"],
+							["OUTLINE"] = "OUTLINE",
+							["MONOCHROMEOUTLINE"] = "MONOCROMEOUTLINE",
+							["THICKOUTLINE"] = "THICKOUTLINE",
+						},
+						set = function(info, value) E.db.enhanceFriendsList.nameFontOutline = value; EFL:EnhanceFriends() end
+					}
+				}
+			},
+			zoneFont = {
+				order = 4,
+				type = "group",
+				name = L["Zone Text Font"],
+				guiInline = true,
+				args = {
+					zoneFont = {
+						order = 1,
+						type = "select", dialogControl = "LSM30_Font",
+						name = L["Font"],
+						values = AceGUIWidgetLSMlists.font,
+						set = function(info, value) E.db.enhanceFriendsList.zoneFont = value; EFL:EnhanceFriends() end
+					},
+					zoneFontSize = {
+						order = 2,
+						type = "range",
+						name = L["Font Size"],
+						min = 6, max = 22, step = 1,
+						set = function(info, value) E.db.enhanceFriendsList.zoneFontSize = value; EFL:EnhanceFriends() end
+					},
+					zoneFontOutline = {
+						order = 3,
+						type = "select",
+						name = L["Font Outline"],
+						desc = L["Set the font outline."],
+						values = {
+							["NONE"] = L["None"],
+							["OUTLINE"] = "OUTLINE",
+							["MONOCHROMEOUTLINE"] = "MONOCROMEOUTLINE",
+							["THICKOUTLINE"] = "THICKOUTLINE",
+						},
+						set = function(info, value) E.db.enhanceFriendsList.zoneFontOutline = value; EFL:EnhanceFriends() end
+					}
+				}
+			}
+		}
+	}
+end
+
+local function ClassColorCode(class)
+	for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do
+		if class == v then
+			class = k
+		end
+	end
+	if Locale ~= "enUS" then
+		for k, v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do
+			if class == v then
+				class = k
+			end
+		end
+	end
+	local color = RAID_CLASS_COLORS[class]
+	if not color then
+		return format("|cFF%02x%02x%02x", 255, 255, 255)
+	else
+		return format("|cFF%02x%02x%02x", color.r*255, color.g*255, color.b*255)
+	end
+end
+
+function EFL:EnhanceFriends()
+	local scrollFrame = FriendsFrameFriendsScrollFrame
+	local buttons = scrollFrame.buttons
+	local numButtons = #buttons
+	local playerZone = GetRealZoneText()
+
+	for i = 1, numButtons do
+		local Cooperate = false
+		local button = buttons[i]
+		local nameText, nameColor, infoText, broadcastText
+
+		if button.buttonType == FRIENDS_BUTTON_TYPE_WOW then
+			local name, level, class, area, connected, status = GetFriendInfo(button.id)
+			broadcastText = nil
+			if connected then
+				if status == "" then
+					button.status:SetTexture(E.db.enhanceFriendsList.enhancedTextures and EnhancedOnline or FRIENDS_TEXTURE_ONLINE)
+				elseif status == CHAT_FLAG_AFK then
+					button.status:SetTexture(E.db.enhanceFriendsList.enhancedTextures and EnhancedAfk or FRIENDS_TEXTURE_AFK)
+				elseif status == CHAT_FLAG_DND then
+					button.status:SetTexture(E.db.enhanceFriendsList.enhancedTextures and EnhancedDnD or FRIENDS_TEXTURE_DND)
+				end
+				local diff = level ~= 0 and format("|cff%02x%02x%02x", GetQuestDifficultyColor(level).r * 255, GetQuestDifficultyColor(level).g * 255, GetQuestDifficultyColor(level).b * 255) or "|cFFFFFFFF"
+				if E.db.enhanceFriendsList.enhancedName then
+					if E.db.enhanceFriendsList.showClass then
+						if E.db.enhanceFriendsList.levelColor then
+							nameText = format("%s%s - %s %s%s|r %s%s", ClassColorCode(class), name, LEVEL, diff, level, ClassColorCode(class), class)
+						else
+							nameText = format("%s%s - %s %s %s", ClassColorCode(class), name, LEVEL, level, class)
+						end
+					else
+						if E.db.enhanceFriendsList.levelColor then
+							nameText = format("%s%s - %s %s%s|r", ClassColorCode(class), name, LEVEL, diff, level)
+						else
+							nameText = format("%s%s - %s %s", ClassColorCode(class), name, LEVEL, level)
+						end
+					end
+				else
+					if E.db.enhanceFriendsList.showClass then
+						if E.db.enhanceFriendsList.levelColor then
+							nameText = format("%s, %s %s%s|r %s", name, LEVEL, diff, level, class)
+						else
+							nameText = format("%s, %s %s %s", name, LEVEL, level, class)
+						end
+					else
+						if E.db.enhanceFriendsList.levelColor then
+							nameText = format("%s, %s %s%s|r", name, LEVEL, diff, level)
+						else
+							nameText = format("%s, %s %s", name, LEVEL, level)
+						end
+					end
+				end
+				nameColor = FRIENDS_WOW_NAME_COLOR
+				Cooperate = true
+			else
+				button.status:SetTexture(E.db.enhanceFriendsList.enhancedTextures and EnhancedOffline or FRIENDS_TEXTURE_OFFLINE)
+				nameText = name
+				nameColor = FRIENDS_GRAY_COLOR
+			end
+			infoText = area
+		end
+
+		if nameText then
+			button.name:SetText(nameText)
+			button.name:SetTextColor(nameColor.r, nameColor.g, nameColor.b)
+			button.info:SetText(infoText)
+			button.info:SetTextColor(0.49, 0.52, 0.54)
+			if Cooperate then
+				if E.db.enhanceFriendsList.enhancedZone then
+					if E.db.enhanceFriendsList.sameZone then
+						if infoText == playerZone then
+							button.info:SetTextColor(0, 1, 0)
+						else
+							button.info:SetTextColor(1, 0.96, 0.45)
+						end
+					else
+						button.info:SetTextColor(1, 0.96, 0.45)
+					end
+				else
+					if E.db.enhanceFriendsList.sameZone then
+						if infoText == playerZone then
+							button.info:SetTextColor(0, 1, 0)
+						else
+							button.info:SetTextColor(0.49, 0.52, 0.54)
+						end
+					else
+						button.info:SetTextColor(0.49, 0.52, 0.54)
+					end
+				end
+			end
+			button.name:SetFont(LSM:Fetch("font", E.db.enhanceFriendsList.nameFont), E.db.enhanceFriendsList.nameFontSize, E.db.enhanceFriendsList.nameFontOutline)
+			button.info:SetFont(LSM:Fetch("font", E.db.enhanceFriendsList.zoneFont), E.db.enhanceFriendsList.zoneFontSize, E.db.enhanceFriendsList.zoneFontOutline)
+		end
+	end
+end
+
+function EFL:FriendDropdownUpdate()
+	local status
+	if IsChatAFK() then
+		status = E.db.enhanceFriendsList.enhancedTextures and EnhancedAfk or FRIENDS_TEXTURE_AFK
+	elseif IsChatDND() then
+		status = E.db.enhanceFriendsList.enhancedTextures and EnhancedDnD or FRIENDS_TEXTURE_DND
+	else
+		status = E.db.enhanceFriendsList.enhancedTextures and EnhancedOnline or FRIENDS_TEXTURE_ONLINE
+	end
+
+	FriendsFrameStatusDropDownStatus:SetTexture(status)
+end
+
+function EFL:FriendListUpdate()
+	hooksecurefunc("HybridScrollFrame_Update", EFL.EnhanceFriends)
+	hooksecurefunc("FriendsFrame_UpdateFriends", EFL.EnhanceFriends)
+	hooksecurefunc("FriendsFrameStatusDropDown_Update", EFL.FriendDropdownUpdate)
+end
+
+function EFL:Initialize()
+	EP:RegisterPlugin(addonName, EFL.InsertOptions);
+
+	EFL:FriendListUpdate()
+end
+
+E:RegisterModule(EFL:GetName())
---
+++ ElvUI_EnhancedFriendsList/Locales.lua
@@ -0,0 +1,127 @@
+-- English
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "enUS", true);
+if not L then return end
+
+L["Enhanced Friends List"] = true;
+L["Enhanced Name"] = true;
+L["Enhanced Status"] = true;
+L["Enhanced Zone"] = true;
+L["Level Range Color"] = true;
+L["Name Text Font"] = true;
+L["Same Zone Color"] = true;
+L["Show Class Text"] = true;
+L["Zone Text Font"] = true;
+
+if GetLocale() == "enUS" then return end
+
+-- German
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "deDE")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- Spanish
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "esES")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- French
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "frFR")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- Korean
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "koKR")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- Portuguese
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "ptBR")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- Russian
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "ruRU")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- Chinese (China, simplified)
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "zhCN")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
+
+-- Chinese (Taiwan, traditional)
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "zhTW")
+if L then
+	L["Enhanced Friends List"] = true;
+	L["Enhanced Name"] = true;
+	L["Enhanced Status"] = true;
+	L["Enhanced Zone"] = true;
+	L["Level Range Color"] = true;
+	L["Name Text Font"] = true;
+	L["Same Zone Color"] = true;
+	L["Show Class Text"] = true;
+	L["Zone Text Font"] = true;
+end
